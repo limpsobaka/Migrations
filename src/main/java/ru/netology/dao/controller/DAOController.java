69efbb7dc7f1a63b0537fbb2d88a128c6a7dcd65
Limpsobaka <limpsobaka@yandex.ru>
2023-04-14 08:19:18 +0300

    Change DAORepository.java

--- src/main/java/ru/netology/dao/controller/DAOController.java
+++ src/main/java/ru/netology/dao/controller/DAOController.java
@@ -1,20 +1,24 @@
 package ru.netology.dao.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.netology.dao.repository.DAORepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products/fetch-product")
 public class DAOController {
-  private DAORepository daoRepository;
+  private final DAORepository daoRepository;
 
   public DAOController(DAORepository daoRepository) {
     this.daoRepository = daoRepository;
   }
 
   @GetMapping()
-  public String getProductByName(@RequestParam("name") String name) {
+  public List<String> getProductByName(@RequestParam("name") String name) {
     return daoRepository.getProductByName(name);
   }
 }
--- src/main/java/ru/netology/dao/repository/DAORepository.java
+++ src/main/java/ru/netology/dao/repository/DAORepository.java
@@ -1,37 +1,31 @@
 package ru.netology.dao.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository
 public class DAORepository {
   private final String SCRIPT = read("getProductsQuery.sql");
-  @Autowired
-  private DataSource dataSource;
-  @Autowired
-  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  public void setDataSource(DataSource dataSource) {
-    this.dataSource = dataSource;
+  public DAORepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
   }
 
-  public String getProductByName(String name) {
+  public List<String> getProductByName(String name) {
     Map<String, String> map = new HashMap<>();
     map.put("name", name);
-    String product = namedParameterJdbcTemplate.query(SCRIPT, map,
-            (rs, rn) -> rs.getString(1)).toString();
-    return product;
+    return namedParameterJdbcTemplate.queryForList(SCRIPT, map, String.class);
   }
 
   private static String read(String scriptFileName) {
